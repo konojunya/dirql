e4b4f91c45b3440e4cb4a80a062d881ceb750036
konojunya <junya03dance@gmail.com>
2019-02-11 15:43:14 +0900

    feat: add parser

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,3 +1,17 @@
-export const parser = (query: string) => {
-  console.log(query)
+import { validate } from "./validator";
+
+export interface ParsedInput {
+  command: string;
+  field: string;
+  key: string;
+  path: string;
+}
+
+/**
+ * query is like this
+ * `select * from ./`
+ */
+export const parser = (query: string): ParsedInput => {
+  const splitedQuery = query.split(" ");
+  return validate(splitedQuery);
 }
---
+++ packages/parser/src/validator.ts
@@ -0,0 +1,77 @@
+import fs from "fs";
+import { ParsedInput } from "./";
+
+// expect
+const expectCommands = ["select"];
+const expectKeys = ["from"];
+const expectFields = [
+  "*",
+  "name",
+  "size",
+  "name,size",
+  "size,name"
+];
+
+export const validate = (q: string[]): ParsedInput => {
+  // expect command length 4
+  if (q.length !== 4) {
+    console.error("The command syntax is incorrect.\nExample: select * from ./");
+    process.exit(1);
+  }
+
+  const [command, field, key, path] = q;
+
+  // expect command
+  if (!isExpectCommandType(command)) {
+    console.error(`invalid command ${command}`);
+    process.exit(1);
+  }
+
+  // expect field
+  if (!isExpectFieldType(field)) {
+    console.error(`invalid field ${field}`);
+    process.exit(1);
+  }
+
+  // expect key
+  if (!isExpectKeyType(key)) {
+    console.error(`invalid key ${key}`);
+    process.exit(1);
+  }
+
+  if (!isValidPath(path)) {
+    console.error(`invalid path ${path}`);
+    process.exit(1);
+  }
+
+  return {
+    command,
+    field,
+    key,
+    path
+  }
+}
+
+const isExpectCommandType = (command: string): boolean => {
+  return !!expectCommands.find(c => c === command);
+}
+
+const isExpectKeyType = (key: string): boolean => {
+  return !!expectKeys.find(k => k === key);
+}
+
+const isExpectFieldType = (field: string): boolean => {
+  return !!expectFields.find(f => f === field);
+}
+
+const isValidPath = (path: string): boolean => {
+  let inValid = false;
+
+  fs.stat(path, (err) => {
+    if (err) {
+      inValid = true
+    }
+  });
+
+  return inValid;
+}
